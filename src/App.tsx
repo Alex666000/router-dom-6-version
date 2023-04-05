df9479b75822a5edca68c4637f83645013c167b8
Aleksandr_UB <bogdanov777000@mail.ru>
2023-04-05 18:09:08 +0300

    Tailwind install + шрифт мой

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,20 @@
 import React from "react";
 import "./App.css";
-import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Link, Outlet,
-    Route, RouterProvider
-} from "react-router-dom";
-import {Home} from "Home";
-import {Data, dataLoader} from "Data";
-import {Contact} from "Contact";
+
 
 
 function App(props: any) {
 
-    const router = createBrowserRouter(
-        createRoutesFromElements(
-            /*корень*/
-            <Route path={"/"} element={<Root/>}>
-                {/*определяем маршруты все*/}
-                <Route index element={<Home/>}/>
-                <Route path={"/data"} element={<Data/>} loader={dataLoader}/>
-                <Route path={"/contact"} element={<Contact/>}/>
-            </Route>
-        )
-    );
-
-
-    return (
+       return (
         <div className="App">
-            <RouterProvider router={router}/>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         </div>
     );
 }
 
 export default App;
-
-const Root = () => {
-    return <>
-        <div>
-            <Link to={"/"}>
-                Home
-            </Link>
-            <Link to={"/data"}>
-                Data
-            </Link>
-        </div>
-        <Outlet/>
-    </>;
-
-};
 
 /*
 
