b1b7db087a1915f551fe0f447e543ecbca40e5cb
Aleksandr_UB <bogdanov777000@mail.ru>
2023-04-05 17:37:10 +0300

    router 6 v + loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Link, Outlet,
+    Route, RouterProvider
+} from "react-router-dom";
+import {Home} from "Home";
+import {Data, dataLoader} from "Data";
+import {Contact} from "Contact";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(props: any) {
+
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            /*корень*/
+            <Route path={"/"} element={<Root/>}>
+                {/*определяем маршруты все*/}
+                <Route index element={<Home/>}/>
+                <Route path={"/data"} element={<Data/>} loader={dataLoader}/>
+                <Route path={"/contact"} element={<Contact/>}/>
+            </Route>
+        )
+    );
+
+
+    return (
+        <div className="App">
+            <RouterProvider router={router}/>
+        </div>
+    );
 }
 
 export default App;
+
+const Root = () => {
+    return <>
+        <div>
+            <Link to={"/"}>
+                Home
+            </Link>
+            <Link to={"/data"}>
+                Data
+            </Link>
+        </div>
+        <Outlet/>
+    </>;
+
+};
+
+/*
+
+ */
